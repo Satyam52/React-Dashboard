bcc6301cabae3c372c65c0b8040d568415c01e13
Abhishek Kumar Gupta <45595749+Satyam52@users.noreply.github.com>
2020-03-19 00:48:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Dashboard from "./components/Dashboard/Dash";
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
