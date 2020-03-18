3674c398de9101c5bf9d612f2e0ba1cbb535b60e
Abhishek Kumar Gupta <45595749+Satyam52@users.noreply.github.com>
2020-03-19 00:45:54 +0530

    Add files via upload

---
+++ build/precache-manifest.639fc358850b5459021d8e3a445ff958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea9fdb52adccbb6888fed8a0cd4cfed",
+    "url": "/Satyam52/React-Dashboardindex.html"
+  },
+  {
+    "revision": "c312a92562f00767dfb9",
+    "url": "/Satyam52/React-Dashboardstatic/css/main.5190d97b.chunk.css"
+  },
+  {
+    "revision": "79522dee509f04524de5",
+    "url": "/Satyam52/React-Dashboardstatic/js/2.96cb334c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Satyam52/React-Dashboardstatic/js/2.96cb334c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c312a92562f00767dfb9",
+    "url": "/Satyam52/React-Dashboardstatic/js/main.0ee29353.chunk.js"
+  },
+  {
+    "revision": "e90aef469c713a1b684e",
+    "url": "/Satyam52/React-Dashboardstatic/js/runtime-main.e15c581b.js"
+  }
+]);
